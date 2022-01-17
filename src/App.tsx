f2913c4cce7ea2e957a3143c2fa558c497dcfb01
TOTE-Club <96588143+TOTE-Club@users.noreply.github.com>
2022-01-17 14:55:14 -0300

    boilerplate upload

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import "./App.css";
+import { useMemo } from "react";
+
+import Minter from "./Minter";
+
+import * as anchor from "@project-serum/anchor";
+import { clusterApiUrl } from "@solana/web3.js";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  getPhantomWallet,
+  getSolflareWallet,
+  getSolletWallet,
+  getMathWallet,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
+import { ThemeProvider, createTheme } from "@material-ui/core";
+
+
+const theme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+const candyMachineId = process.env.REACT_APP_CANDY_MACHINE_ID
+  ? new anchor.web3.PublicKey(process.env.REACT_APP_CANDY_MACHINE_ID)
+  : undefined;
+
+const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
+
+const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
+const connection = new anchor.web3.Connection(rpcHost);
+
+const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
+
+const txTimeout = 30000; // milliseconds (confirm this works for your project)
+
+const App = () => {
+  const endpoint = useMemo(() => clusterApiUrl(network), []);
+
+  const wallets = useMemo(
+    () => [getPhantomWallet(), getSolflareWallet(), getSolletWallet(), getMathWallet() ],
+    []
+  );
+
+  function toggleMenu() {
+    const menu = document.getElementById("mobileNavContainer")!;
+    menu.classList.toggle("open-menu");
+    console.log("pressed");
+  }
+
+  return (
+    <div>
+      <div id="mobileNavContainer" className="mobile-nav">
+        <div className="mobile-nav-close-button" >
+          <img src="/icons/close.svg" alt="" onClick={toggleMenu}/>
+        </div>
+        <ul>
+          <li>
+            <img className="mobile-nav-logo" src="/img/logo.png" alt="" />
+          </li>
+          <li>
+            <a href="/#link1" onClick={toggleMenu}>
+              Link 1
+            </a>
+          </li>
+          <li>
+            <a href="/#link2" onClick={toggleMenu}>
+              Link 2
+            </a>
+          </li>
+          <li>
+            <a href="/#link3" onClick={toggleMenu}>
+              Link 3
+            </a>
+          </li>
+          <li>
+            <a href="/#link4" onClick={toggleMenu}>
+              Link 4
+            </a>
+          </li>
+          <li>
+            <div className="social-icons">
+              <img className="nav-social" src="/icons/twitter.svg" alt="" />
+              <img className="nav-social" src="/icons/discord.svg" alt="" />
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="mobile-menu-button" onClick={toggleMenu}>
+        <img src="/icons/menu.svg" alt="" />
+      </div>
+      <nav>
+        <div className="nav-container">
+          <img className="nav-logo" src="/img/logo.png" alt="" />
+        </div>
+      </nav>
+      <div className="content-wrapper">
+          <header className="card" id="link1">
+            <div style={{ padding: "0 24px 0 24px 0" }}>
+              <h3 className="text-secondary-color">Welcome To</h3>
+              <h1 className="pb-3">TOTE - Loyalty Edition</h1>
+              <p className="text-secondary-color">
+              Exclusive mint for Founders who showed loyalty throughout our building process. TOTE is a community driven collection of NFTs bringing the lore of TOTE to the world. Contribute to the journey of our lore when holding one of our elves. Not only will you be able to contribute to our world, but find a place where you can meet other creatives and investors inside our exclusive club community.
+              </p>
+            </div>
+            <div>
+              <ThemeProvider theme={theme}>
+                <ConnectionProvider endpoint={endpoint}>
+                  <WalletProvider wallets={wallets} autoConnect>
+                    <WalletDialogProvider>
+                      
+                        <Minter
+                          candyMachineId={candyMachineId}
+                          
+                          connection={connection}
+                          startDate={startDateSeed}
+                          txTimeout={txTimeout}
+                          rpcHost={rpcHost}
+                        />
+                      
+                    </WalletDialogProvider>
+                  </WalletProvider>
+                </ConnectionProvider>
+              </ThemeProvider>
+            </div>
+          </header>
+
+      </div>
+    </div>
+  );
+};
+
+export default App;
